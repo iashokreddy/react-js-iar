7567fb99315f127bfb4884e3c0b0ad3f640285f6
iashoreddy <iashok225@gmail.com>
2020-02-01 00:25:35 +0530

    basic input component

--- src/ui/components/Layout/Header.js
+++ src/ui/components/Layout/Header.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import headerStyles from 'ui/static/css/Header/Header.module.css'
 
-export default () => <div className={headerStyles.header}>Header Section</div>
+export default () => <div className={headerStyles.header}>Flight Booking</div>
